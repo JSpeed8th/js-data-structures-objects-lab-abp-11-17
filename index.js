9e334238c7f4035fc99980f0c289f5ac5c82149d
JSpeed8th <jspeed8th@gmail.com>
2017-12-29 04:05:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-const driver = { name:'sam'};
-function updateDriverWithKeyAndValue(obj, key, value) {
-  return Object.assign({}, obj, {[key]: value });
-}
-function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
-  return Object.assign(obj, {[key]: value });
-}
-function deleteFromDriverByKey(obj, key) {
-  const newdriver = Object.assign({}, obj);
-  delete newdriver[key];
-  return newdriver;
-}
-function destructivelyDeleteFromDriverByKey(obj, key) {
-  Object.assign(obj);
-  delete obj[key];
-  return obj;
+const driver = name['sam'];
+
+function updateDriverWithKeyAndValue (object, key, value) {
+  const newDriver = {...driver, key, value};
+  return newDriver;
 }
