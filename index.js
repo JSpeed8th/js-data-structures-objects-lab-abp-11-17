7d1d6be822213bdfa487960709c298eb25c9f07e
JSpeed8th <jspeed8th@gmail.com>
2017-12-29 04:21:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function updateDriverWithKeyAndValue(obj, key, value) {
 function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
   return newDriver = Object.assign(obj, {[key]: value});
 }
+
+function deleteFromDriverByKey(driver, key) {
+  return newDriver = delete driver.key;
+}
